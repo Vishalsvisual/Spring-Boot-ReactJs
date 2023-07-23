45cc1e42577ff08d9f647b6720ce64b3945d7775
Vishal Kumar <vishal.kumar@gameium.com>
2023-07-23 12:46:20 +0530

    #27052023, Committed integrate docker

---
+++ react-springboot-api/test-excute.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.address
+(id, address_line1, address_line2, city, country, state, zip_code)
+VALUES(1, 'Gopinagar', 'Vatva', 'Ahmedabad', 'India', 'Gujarat', '382440');
+
+INSERT INTO public.employees
+(emp_no, birth_date, email_id, first_name, gender, is_address_same, last_name, permanent_address, residential_address)
+VALUES(1, '2023-07-23 07:10:11.248', 'vishals@visual.in', 'vishal', 'M', true, 'kumar', 1, 1);
